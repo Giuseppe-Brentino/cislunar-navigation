8afd9260bdee9033d28956aa4ab82fc5c61731cc
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2025-02-25 11:44:46 +0100

    comments on utils functions

--- FES/utils/addParameters.m
+++ FES/utils/addParameters.m
@@ -1,4 +1,16 @@
 function addParameters(dict_name, param, value)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Add new elements to a data dictionary.
+%
+% Input:
+% dict_name: string - name of the dictionary to modify
+% param: nx1 cell - cell array containing the names of the new fields
+% value: nx1 cell - cell array containing the data of the fields
+%                   listed in param
+%
+% Output:
+% None
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Open or create a Simulink data dictionary
 dictionary = Simulink.data.dictionary.open(dict_name);
@@ -6,8 +18,13 @@ dictionary = Simulink.data.dictionary.open(dict_name);
 % Get the section of the dictionary for design data
 section = getSection(dictionary, 'Design Data');
 
-for i= 1:length(param)
-    addEntry(section,param{i},value{i})
+% Add data to the dictionary
+if length(param)~=length(value)
+    error('input sizes do not agree');
+else
+    for i= 1:length(param)
+        addEntry(section,param{i},value{i})
+    end
 end
-
+% Save changes
 saveChanges(dictionary)
--- FES/utils/propagateParameters.m
+++ FES/utils/propagateParameters.m
@@ -1,4 +1,15 @@
 function propagateParameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Propagate parameters based on input data from a Simulink data dictionary,
+% updating all the correlated entries.
+%
+% Input:
+% None (dictionary values are retrieved within the function)
+%
+% Output:
+% None 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 % Get dictionary data
 params = getParameters('Scenario.sldd',{'time','Environment'});
@@ -8,6 +19,7 @@ Environment = params{2};
 % Update moon orientation and its rate of change
 [eul, eul_dot, jd_interval] = moonOrientation(Environment.Date,time.value);
 
+% Update the Environment structure with the new moon orientation data
 Environment.Moon.orientation = eul;
 Environment.Moon.orientation_dot = eul_dot;
 Environment.Moon.jd_interval = jd_interval;
@@ -16,11 +28,15 @@ Environment.Moon.jd_interval = jd_interval;
 n = Environment.Moon.SH.n.value;
 m = Environment.Moon.SH.n.value;
 [csi, eta, zeta, Cnm, Snm, TestData] = computeSHCoeffs(n,m);
+
+% Update spherical harmonic coefficients in the Environment structure
 Environment.Moon.SH.csi.value = csi;
 Environment.Moon.SH.eta.value = eta;
 Environment.Moon.SH.zeta.value = zeta;
 Environment.Moon.SH.Cnm.value = Cnm;
 Environment.Moon.SH.Snm.value = Snm;
+
+% Update the dictionary with the modified 'Environment' and 'TestData' fields
 updateParameters('Scenario.sldd',{'Environment','TestData'},{Environment,TestData},true);
 end
 
