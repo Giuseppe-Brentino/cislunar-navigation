2e309a8f24dc36e25f86900427fd59bde0e7b644
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2025-03-12 09:19:22 +0100

    test IMU g-related errors

---
+++ FES/tests/Sensors/test_IMU.m
@@ -0,0 +1,12 @@
+classdef test_IMU < matlab.unittest.TestCase
+    
+    methods(Test)
+        % Test methods
+        
+        function unimplementedTest(testCase)
+            testCase.verifyFail("Unimplemented test");
+        end
+        
+    end
+    
+end
