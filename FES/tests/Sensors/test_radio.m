c92129ff3e83f12d0745f208bf6613182ef16a7f
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2025-03-21 16:49:00 +0100

    test radio measurement update

--- FES/tests/Sensors/test_radio.m
+++ FES/tests/Sensors/test_radio.m
@@ -1,13 +1,5 @@
 classdef test_radio < matlab.unittest.TestCase
 
-    methods(TestClassSetup)
-        % Shared setup for the entire test class
-    end
-
-    methods(TestMethodSetup)
-        % Setup for each test
-    end
-
     methods(Test)
         % Test methods
 
@@ -39,6 +31,50 @@ classdef test_radio < matlab.unittest.TestCase
             % Verify that actual values match expected values
             testCase.verifyEqual(actual_points,expected_points)
         end
+
+        function test_measurementUpdate(testCase)
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % Test Measurement Update
+            % The function checks whether the timestamp and range values 
+            % are properly computed and updated when the signal is 
+            % received, ensuring consistency with expected results.
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            % Define timestamps of the spacecrafts positions
+            time = 0:6;
+
+            % Define the position of the transmitting spacecraft over time
+            TX_data = [0 -101 0 0 0 0 -101;
+                       0 0 101 101 101 101 0;
+                       0 0 0 0 0 0 0];
+            
+            % Define the position of the receiving spacecraft over time
+            RX_data = [zeros(3,1), [1000*ones(1,6);zeros(2,6)]];
+
+            % Save the data into timeseries format
+            RX = timeseries(RX_data,time);
+            TX = timeseries(TX_data,time);
+
+            % Moon radius
+            R_M = 100;
+
+            % Run simulation
+            simulation = sim('Models\Radio\test_update.slx','srcWorkspace','current');
+
+            % Extract simulation outputs
+            timestamp = simulation.timestamp;
+            range = simulation.range;
+
+            % Verify timestamp updates correctly
+            testCase.verifyTrue(all(timestamp.Data(timestamp.Time>=2)==2));
+            testCase.verifyTrue(all(timestamp.Data(timestamp.Time<2)==0));
+
+            % Verify range calculations are correct
+            testCase.verifyTrue(all(range.Data(range.Time>=2)==sqrt(101^2+1000^2)));
+            testCase.verifyTrue(all(range.Data(range.Time<2)==0));
+
+        end
+
     end
 
 end
